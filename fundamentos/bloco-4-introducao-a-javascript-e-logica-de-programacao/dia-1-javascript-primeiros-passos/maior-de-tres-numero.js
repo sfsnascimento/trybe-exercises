bce7394e9697e098d0388425e1be26c80a8765ce
Samuel Nascimento <sfsnascimento@gmail.com>
2021-06-23 18:57:53 -0300

    maior de tres numeros

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/maior-de-tres-numero.js
@@ -0,0 +1,11 @@
+let numero1 = 300;
+let numero2 = 100;
+let numero3 = 89;
+
+if (numero1 > numero2){
+  console.log(numero1);
+} else if (numero2 > numero3){
+  console.log(numero2);
+} else {
+  console.log(numero3);
+}
