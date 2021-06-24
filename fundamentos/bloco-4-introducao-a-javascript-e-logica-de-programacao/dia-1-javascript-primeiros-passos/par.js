cdac16e1aac3295eaf63c48746925c651d76618d
Samuel Nascimento <sfsnascimento@gmail.com>
2021-06-24 11:43:17 -0300

    Determina se um entre tres numeros e impar

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/par.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/par.js
@@ -1,3 +1,8 @@
+/*8.Escreva um programa que defina três números em 
+variáveis e retorne true se pelo menos uma das três 
+for par. Caso contrário, ele retorna false .
+*/
+
 let numero1 = 19;
 let numero2 = 40;
 let numero3 = 21;
