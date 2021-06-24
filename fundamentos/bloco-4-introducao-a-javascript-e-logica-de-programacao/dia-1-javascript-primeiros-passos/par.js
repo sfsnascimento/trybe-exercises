807d5cfc02884eec3761b73ba2cba65b80a8192c
Samuel Nascimento <sfsnascimento@gmail.com>
2021-06-24 11:36:09 -0300

    Determina se um dos tres numeros e par

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/par.js
@@ -0,0 +1,9 @@
+let numero1 = 19;
+let numero2 = 40;
+let numero3 = 21;
+
+if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0){
+  console.log(true);
+} else {
+  console.log(false);
+}
