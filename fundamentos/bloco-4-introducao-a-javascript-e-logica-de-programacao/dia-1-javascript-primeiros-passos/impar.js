7cef521b381db54649b77514df4fafd95582a085
Samuel Nascimento <sfsnascimento@gmail.com>
2021-06-24 12:03:49 -0300

    calculo do lucro

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/impar.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/impar.js
@@ -1,5 +1,5 @@
 /*
-Escreva um programa que defina três números em 
+9.Escreva um programa que defina três números em 
 variáveis e retorne true se pelo menos uma das 
 três for ímpar. Caso contrário, ele retorna false .
 */
