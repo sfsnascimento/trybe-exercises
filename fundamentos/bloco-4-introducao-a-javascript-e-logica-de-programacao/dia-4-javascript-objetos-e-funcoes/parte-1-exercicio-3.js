5d0a50ae977c919408cd68c37242b6b2d77de4f3
Samuel Nascimento <sfsnascimento@gmail.com>
2021-06-28 14:26:20 -0300

    exercicio 3 parte 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-1-exercicio-3.js
@@ -0,0 +1,14 @@
+/*
+3.Faça um for/in que mostre todas as chaves do objeto.
+*/
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+
+for (let chaves in info){
+  console.log(chaves);
+}
