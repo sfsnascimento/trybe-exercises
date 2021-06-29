d3cfc5e7877b125fa61e82b3d7d5fc2a744e1d88
Samuel Nascimento <sfsnascimento@gmail.com>
2021-06-28 22:58:53 -0300

    exercicio 4 parte 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-2-exercicio-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-2-exercicio-1.js
@@ -2,9 +2,10 @@
 1.Crie uma função que receba uma string e retorne 
 true se for um palíndromo , ou false , se não for.
 */
-let palindromo = true;
+
 
 function verificaPalindrome(palavra){
+  let palindromo = true;
   for(index = 0; index < palavra.length; index += 1){
     if(palavra[index] !== palavra[palavra.length - 1 - index]){
       palindromo = false;
