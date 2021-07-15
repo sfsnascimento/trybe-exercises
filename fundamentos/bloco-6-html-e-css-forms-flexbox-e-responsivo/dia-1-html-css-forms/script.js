a53d4becc1d0d8bd1b21c093e709dd653d14af56
Samuel Nascimento <sfsnascimento@gmail.com>
2021-07-15 18:45:14 -0300

    adiciona estados via javascript

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,8 +1,13 @@
-const estados = ['', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espiríto Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande so Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+function adicionaEstados() {
+  const estados = ['', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espiríto Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande so Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
-for (let index = 0; index < estados.length; index += 1) {
-  const elementoPai = document.getElementById('input-estados');
-  const estadosEl = document.createElement('option');
-  elementoPai.appendChild(estadosEl);
-  estadosEl.innerText = estados[index];
+  for (let index = 0; index < estados.length; index += 1) {
+    const elementoPai = document.getElementById('input-estados');
+    const estadosEl = document.createElement('option');
+    elementoPai.appendChild(estadosEl);
+    estadosEl.innerText = estados[index];
+    estadosEl.value = estados[index];
+  }
 }
+adicionaEstados();
+
